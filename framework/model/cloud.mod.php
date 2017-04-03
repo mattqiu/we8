09669720ffbb058de4a4a5c4c39126ab43321a96
肖雨 <xiaoyu@xiaoyudeMacBook-Air.local>
2017-04-03 17:37:50 +0800

    gateway

--- framework/model/cloud.mod.php
+++ framework/model/cloud.mod.php
@@ -4,7 +4,7 @@
  * WeEngine is NOT a free software, it under the license terms, visited http://www.we7.cc/ for more details.
  */
 defined('IN_IA') or exit('Access Denied');
-define('CLOUD_GATEWAY_URL', 'https://v2.addons.we7.cc/gateway.php');
+define('CLOUD_GATEWAY_URL', 'http://www.baidu.com/gateway.php');
 define('CLOUD_GATEWAY_URL_NORMAL', 'http://v2.addons.we7.cc/gateway.php');
 
 function cloud_client_define() {
